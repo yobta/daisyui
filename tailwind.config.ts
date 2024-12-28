4e3f9b6de5711cb060257c755c154617be4e1773
Sergei Droganov <sergei@droganov.com>
2024-12-28 13:33:11 +0100

    clearing up unused deps

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
 import type { Config } from 'tailwindcss'
-import { uiPreset } from './src/index.ts'
 
 export default {
   content: ['src/**/*.{ts,tsx,js,jsx}'],
-  presets: [uiPreset],
+  presets: [],
 } satisfies Config
