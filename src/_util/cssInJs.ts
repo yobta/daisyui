1bb46a5af7a723651d24d80d784e028bef0c9e13
Sergei Droganov <sergei@droganov.com>
2024-12-28 01:47:35 +0100

    solved

--- src/_util/cssInJs.ts
+++ src/_util/cssInJs.ts
@@ -1,13 +1,5 @@
-import { parse } from 'postcss'
 import type { CssInJs } from 'postcss-js'
-import { objectify } from 'postcss-js'
 
 export const cssInJs = (css: string): CssInJs => {
-  const replaced = css
-    .replace(/@apply\s+([^\n;]+?)\s*\/\s*/g, '@apply $1/')
-    .replace(/@apply\s+([^\n;]+?)\s*!\s*/g, '@apply $1!')
-  const processed = parse(replaced)
-  const jssObject = objectify(processed)
-
-  return jssObject
+  return JSON.parse(css)
 }
